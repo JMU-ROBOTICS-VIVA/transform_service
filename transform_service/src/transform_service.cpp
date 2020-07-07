a4405d41e3b523b807b5c44ff0ef65bdd41f7d59
Nathan Sprague <nathan.r.sprague@gmail.com>
2020-07-07 16:47:15 -0400

    working version

--- transform_service/src/transform_service.cpp
+++ transform_service/src/transform_service.cpp
@@ -1,89 +1,53 @@
+#include <tf2_ros/transform_listener.h>
+#include <memory>
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 #include "rclcpp/rclcpp.hpp"
 #include "transform_service_srv/srv/transform_pose.hpp"
-#include "geometry_msgs/msg/pose_stamped.hpp"
 
-#include <tf2_ros/transform_listener.h>
-#include "geometry_msgs/msg/transform_stamped.hpp"
-#include <builtin_interfaces/msg/time.hpp>
-#include "rclcpp/time_source.hpp"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+using std::placeholders::_1;
+using std::placeholders::_2;
 
-
-#include <memory>
-
-void transform_pose_callback(const std::shared_ptr<transform_service_srv::srv::TransformPose::Request> request,
-          std::shared_ptr<transform_service_srv::srv::TransformPose::Response>      response)
-{
-  response->target_pose.pose.position.x = request->source_pose.pose.position.x + .1;
-  response->target_pose.pose.position.y = request->source_pose.pose.position.y + .2;
-  response->target_pose.pose.position.z = request->source_pose.pose.position.z + .3;
-
-  response->success = true;
-
-}
-
-int main(int argc, char **argv)
-{
-  rclcpp::init(argc, argv);
-
-  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("transform_server");
-
-
-  rclcpp::Clock::SharedPtr clock = std::make_shared<rclcpp::Clock>(RCL_ROS_TIME);
-  rclcpp::WallRate loop_rate(30);
-
-  tf2_ros::Buffer buffer(clock);
-  buffer.setUsingDedicatedThread(true);
-  tf2_ros::TransformListener tfl(buffer, node, false);
-  
-  rclcpp::Time rclcpp_time = clock->now();
-  tf2::TimePoint tf2_time(std::chrono::nanoseconds(rclcpp_time.nanoseconds()));
-
-  rclcpp::Service<transform_service_srv::srv::TransformPose>::SharedPtr service =
-  node->create_service<transform_service_srv::srv::TransformPose>("transform_server", &transform_pose_callback);
-
-  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Ready to transform a pose.");
-
-
-  auto from_pose = geometry_msgs::msg::PoseStamped();
-  auto to_pose = geometry_msgs::msg::PoseStamped();
-  
-  
-  from_pose.pose.position.x = 1;
-  from_pose.pose.position.y = 2;
-  from_pose.pose.position.z = 3;
-  from_pose.header.frame_id = "wheel_left_link";
-
-
-  
-  int count = 0;
-  
-  while (rclcpp::ok()){
-
-    rclcpp::spin_some(node);
-    loop_rate.sleep();
-    
-    count++;
-    geometry_msgs::msg::TransformStamped  transformStamped;
-    try{
-      buffer.transform(from_pose, to_pose, "base_link");
-      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "%f %f %f", to_pose.pose.position.x,
-		  to_pose.pose.position.y,to_pose.pose.position.z );
-      
-      //transformStamped = buffer.lookupTransform("turtle2", "turtle1",
-      //						tf2_time);
-    }
-    catch (tf2::TransformException & ex) {
-      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "%s%d", ex.what(), count);
-      //rclcpp::sleep_for(&1000);
-      continue;
-    }
-   
+class TransformServer : public rclcpp::Node {
+ public:
+  TransformServer()
+      : Node("transform_service"),
+        buffer_(std::make_shared<rclcpp::Clock>(RCL_ROS_TIME)),
+        tfl_(this->buffer_, this, false) {
+    this->create_service<transform_service_srv::srv::TransformPose>(
+        "transform_service",
+        std::bind(&TransformServer::transform_pose_callback, this, _1, _2));
+    buffer_.setUsingDedicatedThread(true);
+    RCLCPP_INFO(get_logger(), "Ready to transform poses.");
   }
 
+ private:
+  void transform_pose_callback(
+      const std::shared_ptr<transform_service_srv::srv::TransformPose::Request>
+          request,
+      std::shared_ptr<transform_service_srv::srv::TransformPose::Response>
+          response) {
+    try {
+      buffer_.transform(request->source_pose, response->target_pose,
+                        request->target_frame);
+      response->success = true;
+    } catch (tf2::TransformException& ex) {
+      RCLCPP_WARN(get_logger(), "%s", ex.what());
+      response->success = false;
+    }
+  }
 
-  
+  tf2_ros::Buffer buffer_;
+  tf2_ros::TransformListener tfl_;
+  rclcpp::Service<transform_service_srv::srv::TransformPose>::SharedPtr
+      service_;
+};
 
-  rclcpp::spin(node);
+int main(int argc, char** argv) {
+  rclcpp::init(argc, argv);
+  rclcpp::executors::MultiThreadedExecutor executor;
+  auto node = std::make_shared<TransformServer>();
+  executor.add_node(node);
+  executor.spin();
   rclcpp::shutdown();
+  return 0;
 }
